d8bae9dd6d7f16896c739f75e99a6b0c83e1d341
KevPlace <kevinpatrickplace@gmail.com>
2024-01-17 13:28:57 -0500

    Assignment 2a

---
+++ average.py
@@ -0,0 +1,13 @@
+# Author: Kevin Place
+# GitHub username: KevPlace
+# Date: 1/17/2024
+# Description: Asks the user for 5 numbers then averages them
+
+print("Please enter five numbers")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The average of those numbers is:")
+print((num_1 + num_2 + num_3 + num_4 + num_5) / 5)

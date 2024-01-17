5e18001a926a8b477f1e7507d4d89f5ddff55bfc
KevPlace <kevinpatrickplace@gmail.com>
2024-01-17 14:23:04 -0500

    test

---
+++ examples.py
@@ -0,0 +1,11 @@
+number = int(input("Please enter a number: "))
+print("The number you entered was", number)
+print(type(number))
+num= 1
+#num +- 1
+
+print(num)
+x = 1
+x = x + 1
+x = False
+print(x)
